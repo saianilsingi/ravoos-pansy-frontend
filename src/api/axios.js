a22e45c5f01623f16ba6edc6519e6c69605d6264
Sai Anil <saianil2002@gmail.com>
2026-01-22 01:21:55 +0530

    Use env-based API URL for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,18 @@
 import axios from "axios";
 
+{/*this is for local run */}
+{/* 
 const api = axios.create({
   baseURL: "http://localhost:8000/api/",
 });
+*/}
+
+{/*This is for cloudflare pages run*/}
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL + "/api/",
+});
+
+{/*change for here */}
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
